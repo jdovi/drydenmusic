503d375f5f3d7ce1c7dd9467f95c937ef19bf8a2
Jesse Dovi <jdovi@buildingspeak.com>
2016-08-16 16:13:58 -0400

    added sheet_list and book list_pages.  fixed some css.

--- DrydenMusicApp/urls.py
+++ DrydenMusicApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^sheet_list/', views.sheet_list, name='sheet_list'),
+    url(r'^book_list/', views.sheet_list, name='book_list')
 ]
--- DrydenMusicApp/views.py
+++ DrydenMusicApp/views.py
@@ -6,9 +6,32 @@ from .models import music
 # Create your views here.
 @login_required
 def index(request):
-    latest_music_list = music.objects.order_by('added')
-    context = {'latest_music_list': latest_music_list}
+
+    #select the latest song sheets
+    latest_music_list = music.objects.filter(file_type=1)
+    latest_music_list = latest_music_list.order_by('added')[:3]
+    
+    #select the latest song books
+    latest_book_list = music.objects.filter(file_type=2)
+    latest_book_list = latest_book_list.order_by('added')[:3]
+    
+    #create the context variable
+    context = {'latest_music_list': latest_music_list,
+            'latest_book_list': latest_book_list,
+        }
     return render(request, 'DrydenMusicApp/index.html', context)
+    
+def sheet_list(request):
+    l = music.objects.filter(file_type=1)
+    l = l.order_by('added')
+    context = {'sheet_list':l}
+    return render(request, 'DrydenMusicApp/sheet_list.html', context)
+    
+def book_list(request):
+    l = music.objects.filter(file_type=2)
+    l = l.order_by('added')
+    context = {'book_list':l}
+    return render(request, 'DrydenMusicApp/book_list.html', context)
     
 def logout(request):
     lgout(request)
