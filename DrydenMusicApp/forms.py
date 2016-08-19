e4bae0bf5c79a929e4cd4059eb5324cdc2e3a823
Jesse Dovi <jdovi@buildingspeak.com>
2016-08-19 10:44:32 -0400

    mapped the root url to the music app.  got dynamic form fields populating

--- DrydenMusicApp/forms.py
+++ DrydenMusicApp/forms.py
@@ -1,21 +1,17 @@
 from django import forms
 from .models import music
 from django.contrib.auth.models import User
+import pdb
 
 class EmailForm(forms.Form):
 
-    SONG_OPTIONS = []
-    m = music.objects.all()
-    m = m.order_by('-added')
-    for item in m:
-        SONG_OPTIONS.append((item.id, item.title))
-
-    EMAIL_OPTIONS = []
-    u = User.objects.all()
-    for item in u:
-        EMAIL_OPTIONS.append((item.email, item.email))
-    
     song_list = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
-                                         choices=SONG_OPTIONS)
+                                         choices=[])
     email_list = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
-                                         choices=EMAIL_OPTIONS)
+                                         choices=[])
+    
+    #this allows the choice fields to be dynamically populated.
+    def __init__(self,*args, **kwargs):
+        super(EmailForm, self).__init__(*args, **kwargs)
+        self.fields['song_list'].choices = [(x.pk, x.title) for x in music.objects.order_by('-added')]
+        self.fields['email_list'].choices = [(x.email, x.email) for x in User.objects.order_by('email')]
--- drydenmusic/urls.py
+++ drydenmusic/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as authviews
 from DrydenMusicApp import views
+from django.shortcuts import HttpResponseRedirect
 
 urlpatterns = [
     url(r'^music/', include('DrydenMusicApp.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', authviews.login),
     url(r'^logout/$', views.logout,name="logout"),
+    url(r'^$', lambda r: HttpResponseRedirect('music/'))
 ]
